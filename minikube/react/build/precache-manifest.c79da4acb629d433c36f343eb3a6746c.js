207afea3e6fd494f6fee9759e353a2189561adb6
vlukyane <vlukyane@yandex.ru>
2020-02-13 18:47:11 +0300

    add react proj

---
+++ minikube/react/build/precache-manifest.c79da4acb629d433c36f343eb3a6746c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108e15efd42a80e1f53632d835730258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d82e954e131998da61be",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4ccb456caca2ec322985",
+    "url": "/static/js/2.4c644687.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4c644687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82e954e131998da61be",
+    "url": "/static/js/main.db551768.chunk.js"
+  },
+  {
+    "revision": "957d3c1fd2727786313e",
+    "url": "/static/js/runtime-main.c59e30ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
